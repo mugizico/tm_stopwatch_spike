cee997a90c381034cd71f4182b381642956a7c64
Takumi Tsunokake <t.bekkou@gmail.com>
2012-03-29 00:37:00 +0900

    delete tab and fill by space

--- Resources/app/ui/ui.js
+++ Resources/app/ui/ui.js
@@ -1,42 +1,42 @@
 (function(){
-	// 名前空間をさらに機能ごとに分割する
-	// 今回はUIを作成するための機能を集約しているので ui と命名した
-	app.ui = {};
-	
+  // 名前空間をさらに機能ごとに分割する
+  // 今回はUIを作成するための機能を集約しているので ui と命名した
+  app.ui = {};
+
   var style = {
     barColor: '#000',
     backgroundColor: '#000'
   };
 
-	// ファクトリメソッド
-	// アプリケーションを起動させるために必要なタブグループやウインドウを生成する
-	app.ui.createApplicationTabGroup = function(){
-		Titanium.UI.setBackgroundColor('#000');
-		
-		var tabGroup = Ti.UI.createTabGroup();
-		
-		var tab1 = app.ui.createStopWatchTab();
-		var tab2 = app.ui.createHowToPlayTab();
-		
-		tabGroup.addTab(tab1);
-		tabGroup.addTab(tab2);
-		
-		return tabGroup;
-	};
-	
-	// ファクトリメソッド
-	// 「ストップウォッチ」タブを生成する
-	app.ui.createStopWatchTab = function(){
-		var win = Titanium.UI.createWindow({
-			title: 'ストップウォッチ',
+  // ファクトリメソッド
+  // アプリケーションを起動させるために必要なタブグループやウインドウを生成する
+  app.ui.createApplicationTabGroup = function(){
+    Titanium.UI.setBackgroundColor('#000');
+
+    var tabGroup = Ti.UI.createTabGroup();
+
+    var tab1 = app.ui.createStopWatchTab();
+    var tab2 = app.ui.createHowToPlayTab();
+
+    tabGroup.addTab(tab1);
+    tabGroup.addTab(tab2);
+
+    return tabGroup;
+  };
+
+  // ファクトリメソッド
+  // 「ストップウォッチ」タブを生成する
+  app.ui.createStopWatchTab = function(){
+    var win = Titanium.UI.createWindow({
+      title: 'ストップウォッチ',
       barColor: style.barColor,
-			backgroundColor: style.backgroundColor
-		});
-		var tab = Titanium.UI.createTab({
-			icon: 'dark_clock.png',
-			title: 'ストップウォッチ',
-			window: win
-		});
+      backgroundColor: style.backgroundColor
+    });
+    var tab = Titanium.UI.createTab({
+      icon: 'dark_clock.png',
+      title: 'ストップウォッチ',
+      window: win
+    });
 
     var label = Ti.UI.createLabel({
       text: '00:00:00.000',
@@ -52,7 +52,7 @@
       bottom: 30
     });
     win.add(button);
-    
+
     var intervalId = null;
 
     var _startStopwatch = function() {
@@ -95,29 +95,29 @@
       }
     });
 
-		return tab;
-	};
-	
-	// ファクトリメソッド
-	// 「使い方」タブを生成する
-	app.ui.createHowToPlayTab = function(){
-		var win = Titanium.UI.createWindow({
-			title: '使い方',
-      barColor: style.barColor,
-			backgroundColor: style.backgroundColor
-		});
+    return tab;
+  };
 
-		var tab = Titanium.UI.createTab({
-			icon: 'dark_info.png',
-			title: '使い方',
-			window: win
-		});
+  // ファクトリメソッド
+  // 「使い方」タブを生成する
+  app.ui.createHowToPlayTab = function(){
+    var win = Titanium.UI.createWindow({
+      title: '使い方',
+      barColor: style.barColor,
+      backgroundColor: style.backgroundColor
+    });
+
+    var tab = Titanium.UI.createTab({
+      icon: 'dark_info.png',
+      title: '使い方',
+      window: win
+    });
 
     var web = Ti.UI.createWebView({
       url: 'how_to_play.html'
     });
     win.add(web);
 
-		return tab;
-	}
+    return tab;
+  }
 })();
